c68ee065011dec208ef66885b42f40e41d28adaa
kigibigi <b.molchanov.90@gmail.com>
2025-11-30 18:27:15 +0300

    add Connection with database

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Connection mySQLConnection = ConnectionUtil.getConnection();
+        try {
+            System.out.println(mySQLConnection.getTransactionIsolation());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/jm/task/core/jdbc/util/ConnectionUtil.java
@@ -0,0 +1,37 @@
+package jm.task.core.jdbc.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public final class ConnectionUtil {
+    // реализуйте настройку соеденения с БД
+
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+
+    private static void loadProperties() {
+        try(InputStream is = Properties.class.getClassLoader().getResourceAsStream("application.properties")) {
+            Properties p = new Properties();
+            p.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
+        } catch (SQLException sqle) {
+            throw new RuntimeException(sqle);
+        }
+    }
+}
